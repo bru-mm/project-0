a109c1acfafd78f555ee0fcc9fa929aa4dc4f487
Bruno <bruno.monegat@sou.unijui.edu.br>
2023-10-26 02:59:59 +0000

    Removed the SectionContainer component

--- src/components/SectionContainer.tsx
+++
@@ -1,1 +0,0 @@
-// Esse componente vai ser um container para servir como uma lista de secoes. Aqui vai estar definido o paragrafo 'Este e um paragrafo de exemplo', o botao 'Clique Aqui' e uma lista de componentes Section. No exemplo atual so tem uma Section, mas podera ter mais
