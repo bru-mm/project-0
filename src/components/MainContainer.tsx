440da2a56f39dfb43ac8c1365af5e5d65caf18b2
Bruno <bruno.monegat@sou.unijui.edu.br>
2023-10-28 03:09:48 +0000

    Added all the component logic

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,8 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IonButton } from '@ionic/react';
 import Section from './Section.tsx';
 import './MainContainer.css';
 
+const MainContainer: React.FC = () => {
+  const [countries, setCountries] = useState([]);
+  
+  useEffect(() => {
+  	const fetchAllCountries = async () => {
+  		const jsonData = await fetch('https://restcountries.com/v3.1/all?fields=name')
+  			.then((response) => response.json());
+  		const allCountryNames = jsonData.map((country) => country.name.common);
+  		setCountries(allCountryNames);
+  	}
+  	
+  	fetchAllCountries();
+	}, []);
+  
+  const firstSection = {
+    title: 'Seção Importante',
+    imageURL: 'src/assets/images/logo.jpg',
+    description: 'Esta é uma seção com conteúdo relevante.',
+  };
+
+  const [sections, setSections] = useState([firstSection]);
+
+  const onClick = async () => {
+  	// Gera uma seção com uma imagem aleatória, no contexto do tópico, e um Bacon Ipsum
+  	const fetchRandomSection = async (topic) => {
+			return {
+				title: topic,
+				imageURL: await fetch('https://source.unsplash.com/random/?' + topic)
+					.then((response) => response.url),
+				description: await fetch('https://baconipsum.com/api/?type=meat-and-filler&paras=1')
+					.then((response) => response.json()),
+			};
+		};
+  
+  	const randomCountry = countries[Math.floor(Math.random() * countries.length)];
+  	const newSection = await fetchRandomSection(randomCountry);
+    setSections((prevSections) => [newSection, ...prevSections]);
+  };
+
+  return (
+    <div id="main-container">
+      <p id="main-paragraph">Este é um parágrafo de exemplo.</p>
+      <ion-button onClick={onClick} id="main-button">
+        Clique Aqui
+      </ion-button>
+      <div id="main-sections">
+		    {sections.map((section, index) => (
+		      <Section key={index} title={section.title} imageURL={section.imageURL}>
+		        {section.description}
+		      </Section>
+		    ))}
+		   </div>
+    </div>
+  );
+};
+
+/*
 const MainContainer: React.FC = () => {
   const [imageURL, setImageURL] = useState('src/assets/images/logo.jpg');
 
@@ -38,5 +95,6 @@ const MainContainer: React.FC = () => {
     </div>
   );
 };
+*/
 
 export default MainContainer;
